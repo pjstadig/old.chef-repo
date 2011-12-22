83f6724cfed031e6500cd819a24b525278de75f5
Paul Stadig <paul@stadig.name>
2011-12-22 10:05:16 -0500

    Create role for eve.

---
+++ roles/eve.rb
@@ -0,0 +1,8 @@
+name "eve"
+description "Configures eve."
+# List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
+run_list("role[chef-client]")
+# Attributes applied if the node doesn't have it set already.
+#default_attributes()
+# Attributes applied no matter what the node has set already.
+#override_attributes()
