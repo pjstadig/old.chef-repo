4dd981935af2d5d293131d81cde1b650c31bb8c6
Paul Stadig <paul@stadig.name>
2011-12-23 07:12:02 -0500

    Require a password for sudo.

--- roles/eve.rb
+++ roles/eve.rb
@@ -3,10 +3,6 @@ description "Configures eve."
 # List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
 run_list("role[chef-client]", "recipe[eve]", "recipe[users::sysadmins]", "recipe[sudo]", "recipe[java]")
 # Attributes applied if the node doesn't have it set already.
-default_attributes "authorization" => {
-  "sudo" => {
-    "passwordless" => true
-  }
-}
+#default_attributes()
 # Attributes applied no matter what the node has set already.
 #override_attributes()
