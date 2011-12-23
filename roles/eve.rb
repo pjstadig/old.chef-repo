09ca1237146b498d52a74edeef5235b432701f4b
Paul Stadig <paul@stadig.name>
2011-12-22 19:03:25 -0500

    Setup sudo.

--- roles/eve.rb
+++ roles/eve.rb
@@ -1,8 +1,12 @@
 name "eve"
 description "Configures eve."
 # List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
-run_list("role[chef-client]", "recipe[eve]", "recipe[users::sysadmins]")
+run_list("role[chef-client]", "recipe[eve]", "recipe[users::sysadmins]", "recipe[sudo]")
 # Attributes applied if the node doesn't have it set already.
-#default_attributes()
+default_attributes "authorization" => {
+  "sudo" => {
+    "passwordless" => true
+  }
+}
 # Attributes applied no matter what the node has set already.
 #override_attributes()
