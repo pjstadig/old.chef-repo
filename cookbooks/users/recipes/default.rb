d85a36a2fe0026747f11638362d9cfd28d631674
Paul Stadig <paul@stadig.name>
2011-12-24 13:21:36 -0500

    Add user to groups in users cookbook.

--- cookbooks/users/recipes/default.rb
+++ cookbooks/users/recipes/default.rb
@@ -7,9 +7,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -59,5 +59,14 @@ search(:users, '*:*') do |u|
     group u['gid'] || u['id']
     mode "0600"
     variables :ssh_keys => u['ssh_keys']
+  end
+
+  u['groups'].each do |g|
+    if g != "sysdamin"
+      group g do
+        members u['id']
+        append true
+      end
+    end
   end
 end
