606f270b4a4d6cde6ac4d2b6ea94fc0a0e9ace7e
Paul Stadig <paul@stadig.name>
2011-12-30 15:40:50 -0500

    Fix order in eve role.

--- roles/eve.rb
+++ roles/eve.rb
@@ -1,7 +1,7 @@
 name "eve"
 description "Configures eve."
 # List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
-run_list("role[chef-client]", "recipe[eve]", "recipe[users::sysadmins]", "recipe[sudo]")
+run_list("role[chef-client]", "recipe[users::sysadmins]", "recipe[sudo]", "recipe[eve]")
 # Attributes applied if the node doesn't have it set already.
 #default_attributes()
 # Attributes applied no matter what the node has set already.
