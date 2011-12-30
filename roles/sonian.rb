41fe3ee932dad99a1076874886301b94a4180884
Paul Stadig <paul@stadig.name>
2011-12-30 15:48:28 -0500

    Setup an empty sonian role and recipe.

---
+++ roles/sonian.rb
@@ -0,0 +1,8 @@
+name "sonian"
+description "Configures sonian."
+# List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
+run_list("role[chef-client]", "recipe[users::sysadmins]", "recipe[sudo]", "recipe[sonian]")
+# Attributes applied if the node doesn't have it set already.
+#default_attributes()
+# Attributes applied no matter what the node has set already.
+#override_attributes()
