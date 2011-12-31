2e1673b50baefa498b2c63dbba2c9c7a6996fde8
Paul Stadig <paul@stadig.name>
2011-12-31 15:02:37 -0500

    Use the vagrant user instead of sonian.

--- cookbooks/sonian/recipes/default.rb
+++ cookbooks/sonian/recipes/default.rb
@@ -30,30 +30,26 @@ package "unzip"
 package "readpst"
 package "rar"
 
-user "sonian" do
-  uid 7000
-  supports :manage_home => true
-  home "/home/sonian"
-  shell "/bin/bash"
-  password "$1$alefTPi5$fXh04z3ikLmRYf2Ms.3y50"
+user "vagrant" do
+  action :lock
 end
 
-directory "/home/sonian/.ssh" do
-  owner "sonian"
-  group "sonian"
+directory "/home/vagrant/.ssh" do
+  owner "vagrant"
+  group "vagrant"
   mode "0755"
 end
 
-cookbook_file "/home/sonian/.ssh/authorized_keys" do
-  owner "sonian"
-  group "sonian"
+cookbook_file "/home/vagrant/.ssh/authorized_keys" do
+  owner "vagrant"
+  group "vagrant"
   mode "0644"
 end
 
-["/home/sonian/src", "/home/sonian/src/sonian"].each do |d|
+["/home/vagrant/src", "/home/vagrant/src/sonian"].each do |d|
   directory d do
-    owner "sonian"
-    group "sonian"
+    owner "vagrant"
+    group "vagrant"
     mode "0755"
   end
 end
@@ -63,16 +59,16 @@ execute "expand-elasticsearch" do
   command "tar xzf elasticsearch-#{es_version}.tar.gz" +
     " && ln -sf elasticsearch-#{es_version} elasticsearch" +
     " && rm elasticsearch-#{es_version}.tar.gz"
-  cwd "/home/sonian/src/sonian"
-  user "sonian"
+  cwd "/home/vagrant/src/sonian"
+  user "vagrant"
   action :nothing
 end
 
-remote_file "/home/sonian/src/sonian/elasticsearch-#{es_version}.tar.gz" do
+remote_file "/home/vagrant/src/sonian/elasticsearch-#{es_version}.tar.gz" do
   source "https://github.com/downloads/elasticsearch/elasticsearch/elasticsearch-#{es_version}.tar.gz"
-  owner "sonian"
-  group "sonian"
+  owner "vagrant"
+  group "vagrant"
   mode "0600"
   notifies :run, resources(:execute => "expand-elasticsearch"), :immediately
-  not_if { File.exists?("/home/sonian/src/sonian/elasticsearch-#{es_version}") }
+  not_if { File.exists?("/home/vagrant/src/sonian/elasticsearch-#{es_version}") }
 end
